30db21a3e9f7b4eaf25f472506f35520224662d1
Jebakani <vjishwarya@gmail.com>
2021-07-03 16:36:53 +0530

    [JEBAKANI] Creating Address Book Management

---
+++ AddressBook/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Program
+    {
+        /// <summary>
+        /// Welcome to Address Book Management System
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address book");
+            Console.Read();
+        }
+    }
+}
