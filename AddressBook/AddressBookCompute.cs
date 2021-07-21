05cf232a56b61bf0e05fb74674ca22eadfad6ebe
Jebakani <vjishwarya@gmail.com>
2021-07-21 18:03:16 +0530

    [JEBAKANI] Storing unique data in the address book

--- AddressBook/AddressBookCompute.cs
+++ AddressBook/AddressBookCompute.cs
@@ -8,19 +8,30 @@ namespace AddressBook
 {
     class AddressBookCompute
     {
-        private LinkedList<ContactDetails> contactList;
-
+        private List<ContactDetails> contactList;
+        bool AVAILABLE = false;
         //creates the object linked list 
         public AddressBookCompute()
         {
-            this.contactList = new LinkedList<ContactDetails>();
+            this.contactList = new List<ContactDetails>();
         }
 
         //this method add details to the address book
         public  void AddContactDetails(string firstName, string lastName, string address, string city, string state, long zipCode, long phoneNumber)
         {
-            ContactDetails contactDetails = new ContactDetails(firstName, lastName, address, city, state, zipCode, phoneNumber);
-            this.contactList.AddLast(contactDetails);
+            //find the data that already has the same value 
+            ContactDetails details = this.contactList.Find(x => x.firstName.Equals(firstName));
+            //if no sush record is available then add the data
+            if (details==null)
+            {
+                ContactDetails contactDetails = new ContactDetails(firstName, lastName, address, city, state, zipCode, phoneNumber);
+                this.contactList.Add(contactDetails);
+            }
+            //else print record is already available
+            else
+            {
+                Console.WriteLine("record with same name as {0} is available in the address book",firstName);
+            }
         }
 
         //calls the display method
@@ -31,23 +42,28 @@ namespace AddressBook
                 contact.Display();
             }
         }
-
-
         //Delete the particular object
         public void DeleteContact(string name)
         {
+            AVAILABLE = false;
             foreach (ContactDetails contact in this.contactList)
             {
                 if(contact.firstName.Equals(name))
                 {
                     this.contactList.Remove(contact);
+                    AVAILABLE = true;
                     break;
                 }
+            }
+            if(!AVAILABLE)
+            {
+                Console.WriteLine("{0} record not available in the address book",name);
             }
         }
 
         public void EditContact(string name,long number)
         {
+            AVAILABLE = false;
             //checks for every object whether the name is equal the given name
             foreach (ContactDetails contact in this.contactList)
             {
@@ -55,8 +71,15 @@ namespace AddressBook
                 {
                     //calls the setdetail method
                     contact.SetDetail(number);
+                    AVAILABLE = true;
+                    break;
                 }
             }
+            if(!AVAILABLE)
+            {
+                Console.WriteLine("{0} record not available in the address book",name);
+            }
+
         }
 
 
